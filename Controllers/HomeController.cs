da5b197559c5c5f3182cd52470a76ab2a3f3da09
Opilane TTHK <opilane@tthk.ee>
2022-02-22 11:42:13 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Kutse_App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace Kutse_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Ootan sind minu peole! Palun tule!!!";
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 10 ? "Tere hommikust!" : "Tere päevast!";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult Ankeet()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult Ankeet(Guest guest)
+        {
+            E_mail(guest);
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guest);
+            }
+            else { return View(); }
+        }
+
+        public void E_mail(Guest guest)
+        {
+            try
+            {
+                WebMail.SmtpServer = "smtp.gmail.com";
+            }
+        }
+
+        /*public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }*/
+    }
+}
